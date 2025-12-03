09a7510624cd7aeb9da9b161dbf99c62e7d6f988
eric-the-geric <eric.gericke2@gmail.com>
2025-12-03 17:52:20 +0100

    added p1 d3

--- d3/p1.py
+++ d3/p1.py
@@ -2,29 +2,29 @@ def read_file(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
     return content
-
-
+# from stackoverflow (so cool!) https://stackoverflow.com/questions/6422700/how-to-get-indices-of-a-sorted-array-in-python
 
 
 def main():
-    f = read_file("input.txt").removesuffix('\n')
-    ranges = f.split(",")
-    invalid_ids = []
-    for r in ranges:
-        SE = r.split("-")
-        n_start = int(SE[0])
-        n_end = int(SE[-1])
-        for number in range(n_start, n_end+1):
-            number_str = str(number)
-            pointer = len(number_str)//2
-            first = number_str[:pointer]
-            second = number_str[pointer:]
-            if first == second:
-                invalid_ids.append(number)
-    print(sum(invalid_ids))
-
-
-
+    file_path = 'in.txt'  # Replace with your file path
+    content = read_file(file_path)
+    banks = content.splitlines()
+    jolts = 0
+    for bank in banks:
+        bats = [int(c) for c in bank]
+        indices =[i[0] for i in sorted(enumerate(bats), key=lambda x:x[1], reverse=True)]
+        if indices[0] == len(bats) -1:
+            jolt = int(str(bats[indices[1]])+str(bats[indices[0]]))
+            jolts+=jolt
+        else:
+            for ind in indices[1:]:
+                if ind < indices[0]:
+                    continue
+                else:
+                    jolt = int(str(bats[indices[0]])+str(bats[ind]))
+                    jolts+=jolt
+                break
+    print(jolts)
 
 if __name__ == "__main__":
     main()
