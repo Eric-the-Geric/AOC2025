b521b5535993d183ec6a4e9f4408a2493331c9ad
eric-the-geric <eric.gericke2@gmail.com>
2025-12-04 09:06:07 +0100

    completed d3 p2 but I required gippidy's help :'(

--- d3/p2.py
+++ d3/p2.py
@@ -2,34 +2,29 @@ def read_file(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
     return content
-# from stackoverflow (so cool!) https://stackoverflow.com/questions/6422700/how-to-get-indices-of-a-sorted-array-in-python
-
 
 def main():
-    file_path = 'eg.txt'  # Replace with your file path
+    file_path = 'in.txt'  # Replace with your file path
     content = read_file(file_path)
     banks = content.splitlines()
     jolts = 0
     for bank in banks:
-        queue = []
         bats = [int(c) for c in bank]
-        indices =[i[0] for i in sorted(enumerate(bats), key=lambda x:x[1])]
-        for bat in bats:
-            print(f"queue: {queue}")
-            if len(queue) > 11:
-                ind = queue.index(min(queue))
-                print(f"full indices ascending: {indices}")
-                print(f"lowest index of min: {ind}")
-                queue.pop(ind)
-                indices.pop(ind)
-            queue.append(bat)
-
-        battery_array = [str(q) for q in queue]
-        #print(battery_array)
-        batt = "".join(battery_array)
-        #print(batt)
-        jolts += int("".join(battery_array))
+        stack = []
+        n = len(bats)
+        k = 12 # amout of batteries to choose
+        # we have to drop n-k batteries
+        drop = n - k
+        for b in bats:
+            while drop > 0 and len(stack) > 0 and b >stack[-1]:
+                stack.pop()
+                drop -= 1
+            stack.append(b)
+        if len(stack) > k:
+            stack = stack[:k]
+        jolts += int("".join([str(s) for s in stack]))
     print(jolts)
+        
         
 
 
