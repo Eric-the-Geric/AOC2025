0799fe868ed62f3bc2814c7337fdb83a99c5fb54
Eric-the-Geric <eric.gericke2@gmail.com>
2025-12-07 10:16:14 +0100

    added p2 but didn't solve it yet

--- d7/p2.py
+++ d7/p2.py
@@ -1,55 +1,50 @@
-from re import findall
-
 def read_file(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
     return content
 
-def main():
-    content = read_file("in.txt")
-    rows = content.splitlines()
-    calcs = list(rows.pop(-1).replace(" ", ""))
-    board = []
-    for row in rows:
-        new_row = list(row)
-        board.append(new_row)
-    numbers = []
-    for i in range(len(board[0])-1, -1, -1):
-        new_number = ""
-        for j in range(len(board)):
-            #print(board[j][i])
-            new_number+=board[j][i]
-            
-        numbers.append(new_number)
-    #print(numbers)
-    i = len(calcs)-1
-    total = 0
-    n = 0
-    for num in numbers:
-        if any(char.isdigit() for char in num)==False:
-            total+=n
-            n = 0
-            i -= 1
-            continue
-        if n == 0:
-            n = int(num)
-            continue
-        if calcs[i] == "*":
-            print(f"{n} * {num}")
-            n*=int(num)
-        elif calcs[i] == "+":
-            print(f"{n} + {num}")
-            n+=int(num)
+def check_boundry(pos, direction, h, w):
+    y, x = pos[0], pos[1]
+    i, j = direction[0], direction[1]
+    if (y+i > h) or (y+i <0) or (x+j > w) or (x+j < 0):
+        return False
+    else:
+        return True
     
-    #forgot that there isn't a last check so need to add this... i guess it's a little hacky though
-    total += n
-    print(total)
+
+def main():
+    content = read_file("eg.txt")
+    rows = content.splitlines()
+    board = [list(line) for line in rows]
+    down = (+1, 0)
+    downright = (+1, +1)
+    downleft = (+1, -1)
+    s = "S"
+    timelines = 0
+    for i, row in enumerate(board):
+        for j, val in enumerate(row):
+            if i == 0:
+                if val == "S":
+                    board[i+down[0]][j+down[1]] = "|"
+            if val == "|":
+                if check_boundry((i+1, j), downright, len(board), len(board[0])):
+                    if board[i+down[0]][j+down[1]] == "^":
+                        timelines+=2
+                        if check_boundry((i+1, j), downright, len(board), len(board[0])):
+                            dr = board[i+downright[0]][j + downright[1]]
+                            if dr == ".":
+                                board[i+ downright[0]][j + downright[1]] = "|"
+                        if check_boundry((i, j), downleft, len(board), len(board[0])):
+                            dl = board[i+ downleft[0]][j + downleft[1]]
+                            if dl == ".":
+                                board[i+ downleft[0]][j + downleft[1]] = "|"
+                    else:
+                        board[i+down[0]][j+down[1]] = "|"
 
 
-
-
-
-
+    print(timelines)
+                    
 
 if __name__ == "__main__":
     main()
+
